8c5b261972357c36543dfed61441aad45b3ce9b9
THLAL16 <tlalancette@geckoal.com>
2024-07-29 15:01:37 -0400

    before split of plot and format

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ import json
 
 # create a json file with the channel configuration
 # the json file should have the following structure:
-# "Channel name ass written in the .csv file": {
+# "Channel name as written in the .csv file": {
 #     "type": (str) variable type orp/pH/temp, TODO: implement use
 #     "label": (str) text to display,
 #     "unit": (str) unit of the original measurement,
 #     "displayUnit": (str) unit to convert the original measurement to TODO: implement conversion
 # },
 
+
 def setupParser():
-    parser = argparse.ArgumentParser(
-        description="Utility to record samples from the Poseidon"
-    )
+    print("parsing arguments")
+    parser = argparse.ArgumentParser(description="Utility to record samples from the Poseidon")
     parser.add_argument(
         "-p",
         "--path",
@@ -74,131 +74,246 @@ def setupParser():
 
     return args
 
-def openFile(path, delimiter:str = ";"):
+
+def openFile(path, delimiter: str = ";"):
+    print(f"opening {path.split('/')[-1]}")
     with open(path, "r") as f:
         data = f.readlines()
         if "," in data[1]:
-            data = [x.replace(",",".") for x in data]
+            data = [x.replace(",", ".") for x in data]
         data = [x.strip().split(delimiter) for x in data]
-        data = np.array(data).T
+        if "" in data[1]:
+            data.pop(1)
+        if "" in data[-1]:
+            data.pop(-1)
+        if len(data[-1]) != len(data[-2]):
+            data.pop(-1)
+        try:
+            data = np.array(data).T
+        except ValueError:
+            if len(data[0]) != len(data[-1]):
+                data.pop(-1)
+            data = np.array(data).T
 
     return data
 
-def loadFile(path, args):
 
-    data = None
+def loadFile(path, args):
+    print(f"loading all .csv in {path}")
+    files = []
 
     # Check if the path is a file or a directory
     if os.path.isfile(path):
-        data = openFile(path, args.delimiter)
+        # data = openFile(path, args.delimiter)
+        files.append(path)
 
     if os.path.isdir(path):
-        # Check if the directory contains csv files
-        for file in os.listdir(path):
-            if not file.endswith(".csv"):
+        # List all the csv files in the directory and subdirectories
+
+        for root, dirs, subfiles in os.walk(path):
+            for subfile in subfiles:
+                if not subfile.endswith(".csv"):
+                    continue
+                if subfile == "config.csv":
+                    continue
+                files.append(os.path.join(root, subfile))
+
+        # if no csv files were found
+        if len(files) == 0:
+            print("No csv files found in the provided directory")
+            exit()
+
+    loadedfiles = {}
+    data = {
+        "dataSize": 0,  # the amount of data points in set
+        "channels": [],  # the channels in the set
+        "files": {
+            #   "filename":{
+            #       "range":[startindex, endIndex],
+            #       "channels":{
+            #           channelName: [...],
+            #           otherChannelName: [...]
+            #       }
+            #   }
+        },
+    }
+
+    # sort the files by name reversed (oldest first)
+    files.sort()
+
+    for file in files:
+        loadedfiles.update({file: []})
+        fileNameShorted = file.split("/")[-1].split(".")[0].split("\\")[1]
+        print(f"loading {files.index(file)}: {fileNameShorted}")
+        fileData = openFile(file, args.delimiter)
+
+        # check if the file is empty
+        if len(fileData) == 0:
+            print(f"{file} is empty")
+            continue
+        # check if the file has more data than just the header
+        if len(fileData[1]) < 2:
+            print(f"{file} has no data")
+            continue
+
+        # check if the file is a duplicate
+        # done by checking if first and last timestamps are already in the data
+        for serie in data["files"]:
+            if (
+                data["files"][serie]["channels"]["Time"][0] == fileData[1][1]
+                and data["files"][serie]["channels"]["Time"][-1] == fileData[1][-1]
+            ):
+                print(f"{file} is a duplicate of {serie}")
                 continue
-            if file == "config.csv":
-                continue
-            data = openFile(os.path.join(path, file), args.delimiter)
 
-        # did not find any csv files in the directory
-        # looking in the subdirectories
+        # get the amount of rows in the file
+        rows = len(fileData[0]) - 1
+        # grab the start and end index of the data
+        startindex = data["dataSize"]
+        endindex = startindex + rows
+        # increment the data size
+        data["dataSize"] += rows
+        # add the data to the channels dict
+        data["files"].update({fileNameShorted: {"range": [startindex, endindex], "channels": {}}})
+        for channel in fileData:
+            if channel[0] not in data["channels"]:
+                data["channels"].append(channel[0])
+            data["files"][fileNameShorted]["channels"][channel[0]] = channel[1:]
 
-        if data is None:
-            # create a list of all the files in the directory
-            files = []
-            for root, dirs, subfiles in os.walk(path):
-                for subfile in subfiles:
-                    if not subfile.endswith(".csv"):
-                        continue
-                    if subfile == "config.csv":
-                        continue
-                    files.append(os.path.join(root, subfile))
-
-            # if no csv files were found
-            if len(files) == 0:
-                print("No csv files found in the provided directory")
-                exit()
-
-            # if multiple csv files were found
-            elif len(files) == 1:
-                data = openFile(files[0], args.delimiter)
-            else:
-                print("TODO: Implement multiple file support")
     return data
 
+
 def loadChannelConfig(path):
+    print(f"loading channel configuration from {path}")
     with open(path, "r") as f:
         data = json.load(f)
     return data
 
-def extractUnit(label:str):
+
+def extractUnit(label: str):
+    print(f"extracting unit from {label}")
     if "[" in label:
         unitLabel = label.split("[")[-1].split("]")[0]
     else:
         unitLabel = label.split("(")[-1].split(")")[0]
     return unitLabel
 
-def plotData(data, args, channelConfig:dict=None):
-    labels = data[:, 0]
-    data = data[:, 1:]
-    # x = np.array(data[0], dtype=float)
-    time = []
-    for timestamp in data[1]:
-        time.append(datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S:%f"))
-    time = np.array(time)
 
-    validIdx = np.where(time != "None")
-    startIdx = np.arange(len(time))
-    endIdx = np.arange(len(time))
-    # list points outside the time range
+def trimData(data: dict, args):
+    NvalidIdx = 0
+
     if args.start is not None:
         startTime = datetime.strptime(args.start, "%Y-%m-%d %H:%M")
-        startIdx = np.where(time[validIdx] > startTime)
-
-
+    else:
+        startTime = None
     if args.end is not None:
         endTime = datetime.strptime(args.end, "%Y-%m-%d %H:%M")
-        endIdx = np.where(time[validIdx] < endTime)
+    else:
+        endTime = None
 
-    validIdx = np.intersect1d(startIdx, endIdx)
+    for serie in data["files"]:
+        print(f"Trimming out of range data of serie {serie}")
+        time = []
 
-    if len(validIdx) == 0:
+        for timestamp in data["files"][serie]["channels"]["Time"]:
+            time.append(datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S:%f"))
+        time = np.array(time)
+
+        validIdx = np.where(time != "None")
+        startIdx = np.arange(len(time))
+        endIdx = np.arange(len(time))
+
+        if startTime is not None:
+            startIdx = np.where(time[validIdx] > startTime)
+        if endTime is not None:
+            endIdx = np.where(time[validIdx] < endTime)
+
+        validIdx = np.intersect1d(startIdx, endIdx)
+
+        NvalidIdx += len(validIdx)
+        data["files"][serie]["validIdx"] = validIdx
+        data["files"][serie]["channels"]["Time"] = time
+
+    if NvalidIdx == 0:
         print("No valid data in the time range")
         exit()
 
-    plots = {}
-    labels: list[str] = labels.tolist()
+
+def plotData(data: dict, args, channelConfig: dict = None):
+    print("plotting data")
+    labels = data["channels"]
+
+    trimData(data, args)
+
+    concatenatedTime = []
+    for serie in data["files"]:
+        concatenatedTime += data["files"][serie]["channels"]["Time"].tolist()
+
     label: str
-    # linestyle = ["-"]*10 + ["--"]*10 + [":"]*10 + ["-."]*10
-    # linestyle_i = iter(linestyle)
-
+    plots = {}
     for label in labels:
-        if channelConfig is not None:
-            if label not in channelConfig.keys():
-                continue
+        # check if the label is in the channel configuration, if not go to the next label
+        if label not in channelConfig.keys():
+            continue
+        elif label != "Time":
             unitLabel = channelConfig[label]["displayUnit"]
+            actualUnit = channelConfig[label]["unit"]
             displayLabel = channelConfig[label]["label"]
-        else:
-            if not "(" in label:
+            if unitLabel not in plots:
+                newAxis: plt.Axes
+                newfig, newAxis = plt.subplots()
+                newAxis.set_xlabel("Time")
+                newAxis.set_ylabel(f"{unitLabel}({actualUnit})")
+                newAxis.set_title(unitLabel)
+                plots.update({unitLabel: (newfig, newAxis)})
+
+        # combine the individual data sets into one easy to index
+        indexChain = []
+        concatenatedData = []
+        for serie in data["files"]:
+            if label not in data["files"][serie]["channels"]:
                 continue
-            unitLabel = extractUnit(label)
-            displayLabel = label
-        if unitLabel not in plots:
-            plots.update({unitLabel: plt.subplots()})
+            serieValidIdx = data["files"][serie]["validIdx"]
+            if len(serieValidIdx) == 0:
+                continue
+            indexChain.append(
+                [
+                    data["files"][serie]["range"][0] + serieValidIdx[0],
+                    data["files"][serie]["range"][0] + serieValidIdx[-1] + 1,
+                ]
+            )
+            concatenatedData += data["files"][serie]["channels"][label][serieValidIdx].tolist()
 
-        unit = np.array(data[labels.index(label)], dtype=float)
-        plots[unitLabel][1].plot(time[validIdx], unit[validIdx], label=f"{label}-{displayLabel}")
-        # plots[unitLabel][1].plot(time[validIdx], unit[validIdx], label=unitLabel, linestyle=next(linestyle_i))
+        # check if the index chain is correct
+        for link in range(len(indexChain) - 1):
+            if indexChain[link][1] != indexChain[link + 1][0]:
+                print("Index chain is broken and sparse data is not supported yet")
+                print(f"label: {label}")
+                print(f"Broken chain between {indexChain[link][1]} and {indexChain[link + 1][0]}")
+                # print the time of the broken chain
+                print(
+                    f"Time of the broken chain: {data['files'][serie]['channels']['Time'][indexChain[link][1]]} and {data['files'][serie]['channels']['Time'][indexChain[link + 1][0]]}"
+                )
+                exit()
 
-    
-    for key in plots:
-        plots[key][1].legend()
-        plots[key][1].set_xlabel("Time")
-        plots[key][1].set_ylabel(key)
-        plots[key][1].set_title(args.path.split("/")[-1])
+        # plot the data
+        if label != "Time":
+            print(f"plotting {len(concatenatedData)} points for {label}")
+            if unitLabel == "ORP":
+                yData = np.array(concatenatedData, float)*1000
+            else:
+                yData = np.array(concatenatedData, float)
+            plots[unitLabel][1].plot(
+                concatenatedTime[indexChain[0][0] : indexChain[-1][1]],
+                yData,
+                label=f"{label}-{displayLabel}",
+            )
+
+    for plot in plots:
+        currAxis:plt.Axes = plots[plot][1]
+        currAxis.legend()
+
     plt.show()
-
 
 def main():
     args = setupParser()
